4c18087eb39d6ace25340bafee4b861588add97e
Martí Adrogué <marti.adrogue@gmail.com>
2016-09-10 20:09:53 +0200

    Update pacakges

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,6 +12,24 @@ module.exports = function (grunt) {
         },
       },
     },
+    sass: {
+      dev: {
+        options: {
+          outputStyle: 'expanded',
+        },
+        files: {
+          'assets/css/styles.css': 'assets/scss/source.scss',
+        },
+      },
+      build: {
+        options: {
+          outputStyle: 'compressed',
+        },
+        files: {
+          'assets/css/styles.css': 'assets/scss/source.scss',
+        },
+      },
+    },
     cssmin: {
       options: {
         keepSpecialComments: 0,
@@ -25,22 +43,6 @@ module.exports = function (grunt) {
             'assets/css/styles.css',
           ],
         },
-      },
-    },
-    concat: {
-      js: {
-        src: ['assets/js/block/**/*.js'],
-        dest: 'assets/js/build.js',
-      },
-      resource: {
-        files: [
-          {
-            expand: true,
-            cwd: 'assets/js/raw',
-            src: '**/*.json',
-            dest: 'js/raw',
-          },
-        ],
       },
     },
     uglify: {
@@ -59,9 +61,8 @@ module.exports = function (grunt) {
         },
       },
       build: {
-        files: {
-          'js/main.js': ['assets/js/build.js'],
-        },
+        src: ['assets/js/block/**/*.js'],
+        dest: 'js/build.js',
       },
       lib: {
         files: [
@@ -70,6 +71,16 @@ module.exports = function (grunt) {
             cwd: 'assets/js/lib',
             src: '**/*.*',
             dest: 'js',
+          },
+        ],
+      },
+      resource: {
+        files: [
+          {
+            expand: true,
+            cwd: 'assets/js/raw',
+            src: '**/*.json',
+            dest: 'js/raw',
           },
         ],
       },
@@ -98,13 +109,17 @@ module.exports = function (grunt) {
         files: 'assets/less/**/*.less',
         tasks: ['less'],
       },
+      sass: {
+        files: 'assets/scss/**/*.scss',
+        tasks: ['sass:dev'],
+      },
       css: {
         files: 'assets/css/**/*.css',
         tasks: ['cssmin'],
       },
       js: {
         files: 'assets/js/**/*.js',
-        tasks: ['concat', 'uglify'],
+        tasks: ['uglify'],
       },
       img: {
         files: 'assets/img/**/*.*',
@@ -121,8 +136,8 @@ module.exports = function (grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-watch');
